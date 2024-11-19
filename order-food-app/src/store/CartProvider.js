40ddb1e9b9c26f9890b0e32ed5721a50f435ff7f
Nikolay <palmer@bk.ru>
2024-11-19 16:00:51 +0300

    working with useReducer for cart (lesson 144)

--- order-food-app/src/store/CartProvider.js
+++ order-food-app/src/store/CartProvider.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import CartContext from "./cart-contex";
+
+//значение по умолчанию
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.amount * action.item.price
+        return ({
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        })
+    }
+    if (action.type === 'REMOVE_ITEM') {
+
+    }
+    return defaultCartState
+
+}
+
 
 const CartProvider = (props) => {
 
+
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+
     const addItemToCarthandler = (item) => {
+        dispatchCartAction({type: 'ADD_ITEM', item: item})
 
     }
 
     const removeItemFromCarthandler = (id) => {
-
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id})
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCarthandler,
         removeItem: removeItemFromCarthandler
     }
