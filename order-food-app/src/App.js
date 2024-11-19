19812dbf0ec152314e936ff3f81a784fada2525d
Nikolay <palmer@bk.ru>
2024-11-19 11:46:49 +0300

    finished portal implementing without logic component (140 lesson) no logic yet also in app

--- order-food-app/src/App.js
+++ order-food-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from "./components/Layout/Header/Header";
 import Meal from "./components/Layout/Meal/Meal";
+import Cart from "./components/Layout/Cart/Cart";
 
 function App() {
     return (
         <>
+            <Cart/>
             <Header/>
-
             <main>
                 <Meal/>
             </main>
