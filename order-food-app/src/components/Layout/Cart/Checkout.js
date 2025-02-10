0ba5aa291866b48c63f0b34970128abc1e366275
Nikolay <palmer@bk.ru>
2025-02-10 12:32:44 +0300

    220 changed styles and add ref for input fields for getting data form them, avoid use useState for each field in checkout form

--- order-food-app/src/components/Layout/Cart/Checkout.js
+++ order-food-app/src/components/Layout/Cart/Checkout.js
@@ -1,33 +1,47 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import classes from './Checkout.module.css'
 
 
 const Checkout = (props) => {
 
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalInputRef = useRef();
+    const cityInputRef = useRef();
+
     const confirmHandler = (event) => {
-     event.preventDefault();
+        event.preventDefault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostal = postalInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+
     }
 
     return (<form onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name"></input>
+                <input type="text" id="name" ref={nameInputRef}></input>
 
             </div>
             <div className={classes.control}>
                 <label htmlFor="street">Your Address</label>
-                <input type="text" id="street"></input>
+                <input type="text" id="street" ref={streetInputRef}></input>
             </div>
             <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
-                <input type="text" id="postal"></input>
+                <input type="text" id="postal" ref={postalInputRef}></input>
             </div>
             <div className={classes.control}>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city"></input>
+                <input type="text" id="city" ref={cityInputRef}></input>
             </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button>Confirm</button>
+            <div className={classes.actions}>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button>Confirm</button>
+            </div>
+
         </form>
     )
 }
